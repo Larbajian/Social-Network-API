ec6d8c3fb43d6a2f79d7aa9a8e2e98795bc39208
unknown <lexiarbajianclt@gmail.com>
2023-02-23 11:56:02 -0500

    partial thought model

--- models/thought.js
+++ models/thought.js
@@ -2,13 +2,36 @@ const { triggerAsyncId } = require('async_hooks');
 const { Schema, model } = require('mongoose');
 
 const thoughtSchema = new Schema(
-    {
-        thoughtText: { type: String, minlength: 1, maxlength: 280, required: true },
-        createdAt: { type: Date, default: Date.now, get: formatDate }
-        username: {}
-    }
-
-)
+  {
+    thoughtText: {
+      type: String,
+      minlength: 1,
+      maxlength: 280,
+      required: true,
+    },
+    createdAt: {
+      type: Date,
+      default: Date.now,
+      get: formatDate,
+    },
+    username: {
+      type: Schema.Types.ObjectId,
+      ref: "user",
+    },
+    reactions: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: "reaction",
+      },
+    ],
+  },
+  {
+    toJSON: {
+      virtuals: true,
+    },
+    id: false,
+  }
+);
 
 
 
